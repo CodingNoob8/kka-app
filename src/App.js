c51028d77d2a7acb8071c7d454cabb905655d0c8
CodingNoob8 <107682560+CodingNoob8@users.noreply.github.com>
2022-08-26 16:12:01 +0530

    Add
    Add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useForm, FormProvider } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import schema from "./schema/schema";
+
+import Header from "./components/Header";
+import PersonalInformation from "./components/PersonalInformation";
+import Packages from "./components/Packages";
+import Registration from "./components/Registration";
+import GrandTotal from "./components/GrandTotal";
+import Members from "./components/Members";
+import Submit from "./components/Submit";
+
+const INITIAL_VALUES = {
+  salution: 'Mr',
+  firstname:'karthik'
+}
 
 function App() {
+  
+  const methods = useForm({
+    defaultValues: INITIAL_VALUES,
+    mode: "onTouched", // "onChange"
+    resolver: yupResolver(schema)
+
+});
+
+const onSubmit = (data) => {
+  console.log("submitting");
+  console.log(JSON.stringify(data));
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FormProvider {...methods} >
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+      <Header />
+      <PersonalInformation />
+       <Packages />
+      {/*<Registration />*/}
+      <GrandTotal />
+      {/*<Members /> */}
+      {/* <Submit /> */}
+      <input type="submit"/>
+    </form>
+
+    </FormProvider>
+    
   );
 }
 
